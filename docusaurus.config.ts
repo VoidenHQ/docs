4de6f17b19433d5f3cdd6935cf5b60cad6e0cbba
Davinder Singh <davinder@apyhub.com>
2026-06-24 13:34:43 +0530

    implement the search bar (#65)

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -3,6 +3,21 @@ import type { Config } from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
 const config: Config = {
+  plugins: [
+    [
+      require.resolve('@easyops-cn/docusaurus-search-local'),
+      {
+        hashed: true,
+        docsRouteBasePath: '/docs',
+        indexDocs: true,
+        indexBlog: false,
+        indexPages: false,
+        searchResultLimits: 8,
+        searchResultContextMaxLength: 50,
+        searchBarShortcutHint: true,
+      },
+    ],
+  ],
   title: 'Voiden',
   tagline: 'You may need this one day',
   favicon: 'img/favicon.ico',
@@ -119,13 +134,19 @@ const config: Config = {
           label: 'Docs',
         },
         {
+          type: 'search',
+          position: 'right',
+        },
+        {
           position: 'right',
           type: 'html',
+          className: 'navbar__icon-link',
           value: '<a href="https://github.com/VoidenHQ/feedback" target="_blank" style="display:flex;align-items:center;"  class = "navbar__link" ><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-github-icon lucide-github"><path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4"/><path d="M9 18c-4.51 2-5-2-7-2"/></svg></a>',
         },
         {
           type: 'html',
           position: 'right',
+          className: 'navbar__icon-link',
           value: '<a href="https://discord.gg/XSYCf7JF4F" target="_blank" style="display:flex;align-items:center;"  class = "navbar__link" ><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-message-square-icon lucide-message-square"><path d="M22 17a2 2 0 0 1-2 2H6.828a2 2 0 0 0-1.414.586l-2.202 2.202A.71.71 0 0 1 2 21.286V5a2 2 0 0 1 2-2h16a2 2 0 0 1 2 2z"/></svg></a>'
         },
       ],
